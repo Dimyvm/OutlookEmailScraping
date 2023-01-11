21802cbd657d987dc5ef0ff4160a58583aba40fc
Dimyvm <dimitryvanmulders@gmail.com>
2023-01-11 21:05:01 +0100

    update excelController

--- excelController.py
+++ excelController.py
@@ -2,33 +2,53 @@ import os
 import openpyxl
 
 
-def openExcel():
+def getPath():
+
     # Give the location of the file
-    osPath = os.getcwd()
-    print("Current working directory:", str(osPath))
-    file = '\delivery.xlsx'
+    osPath = os.getcwd() #get path of this directory
+    file = '\delivery.xlsx' #filename
+    return osPath+file
+
+
+def openExcel():
+    
+    path = getPath()
 
     # To open the workbook
     # workbook object is created
-    workbook = openpyxl.load_workbook(osPath+file)
-
+    workbook = openpyxl.load_workbook(path)
+    print('file is openend')
+    return workbook
+    
+def readDataFromExcel(workbook, data):
+    
     # Get sheet names
     sheet = workbook.active
-
-    # Get current active sheet
-    # from the active attribute
-    # sheet.title
-
+    
     # Read a Cell
     # Get cell value
     cell_obj = sheet.cell(row=1, column=1)
-
+    
     # Print value of cell
     print(cell_obj.value)
+    
+def deleteArticleExpiration():
+    print('delete Article expiration')
 
-    # Write to a Cell
-    # write to a particular cell
-    sheet.cell(row=2, column=2).value = "Welcome"
+def writeDataToExcel(workbook, articleObjectList):
+    
+    # Get sheet names
+    sheet = workbook.active
+    
+    for articleObject in articleObjectList:
+        max_row = sheet.max_row # find last row of worksheet
+        # Write to a Cell
+        # write to a particular cell
+        sheet.cell(row= max_row+1, column=1).value = articleObject.bestelbonnr
+        sheet.cell(row= max_row+1, column=2).value = articleObject.articleId, 
+        sheet.cell(row= max_row+1, column=3).value = articleObject.number
+        sheet.cell(row= max_row+1, column=4).value = articleObject.deliveryDate
 
+    path = getPath()
     # Save the file
-    workbook.save(osPath+file)
+    workbook.save(path)
